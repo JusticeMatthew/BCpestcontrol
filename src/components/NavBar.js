14cfb0ecedc942230df62b88df3671f4feb73782
unknown <matthewajustice@gmail.com>
2021-07-28 20:27:38 -0400

    Started scroll feature

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { Link, Events, animateScroll as scroll } from 'react-scroll';
 
 export default function TopLogo() {
+  useEffect(() => {
+    Events.scrollEvent.register('begin', function () {
+      console.log('begin', arguments);
+    });
+
+    Events.scrollEvent.register('end', function () {
+      console.log('end', arguments);
+    });
+  }, []);
+
   return (
     <div className='bg-dark flex justify-center w-full items-center mb-12'>
       <div className='flex justify-evenly items-center w-3/4'>
         <ul className='flex justify-around text-light text-xl'>
           <NavItem>About Us</NavItem>
-          <NavItem>Service Area</NavItem>
+          <NavItem className='SA'>
+            <Link
+              activeClass='active'
+              to='SA'
+              spy={true}
+              smooth={true}
+              duration={1000}
+              offset={500}
+            >
+              Service Area
+            </Link>
+          </NavItem>
           <NavItem>Reviews</NavItem>
           <NavItem>Contact Us</NavItem>
         </ul>
