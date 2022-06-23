b549bdb18a817469b20c874fed4a708d5f5058d8
unknown <matthewajustice@gmail.com>
2022-06-23 19:06:06 -0400

    updated copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className='h-14 bg-dark text-light text-center flex justify-center items-center text-sm'>
-      <p>Copyright © 2021 B&C Pest Control</p>
+      <p>Copyright © 2022 B&C Pest Control</p>
     </div>
   );
 }
